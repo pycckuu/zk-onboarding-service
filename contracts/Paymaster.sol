35c0ee04165e8319058c8ee29a38e39d4ff3421c
pycckuu <is.markelov@gmail.com>
2022-10-29 18:54:19 +0100

    paymaster smart contract

--- contracts/Paymaster.sol
+++ contracts/Paymaster.sol
@@ -1,22 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IPaymaster, ExecutionResult} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IPaymaster.sol";
-import {IPaymasterFlow} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IPaymasterFlow.sol";
-import {TransactionHelper, Transaction} from "@matterlabs/zksync-contracts/l2/system-contracts/TransactionHelper.sol";
+import {IPaymaster, ExecutionResult} from '@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IPaymaster.sol';
+import {IPaymasterFlow} from '@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IPaymasterFlow.sol';
+import {TransactionHelper, Transaction} from '@matterlabs/zksync-contracts/l2/system-contracts/TransactionHelper.sol';
 
-import "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
+import '@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol';
 
-import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import '@openzeppelin/contracts/interfaces/IERC20.sol';
 
 contract Paymaster is IPaymaster {
-    uint256 constant N_FIRST_TX = 3;
-    uint256 TX_COUNT = 0;
+    // mapping of the sponsored addresses to the N of free transactions
+    mapping(address => uint256) public nFirstTx;
+    // mapping of the sponsored addresses to the sponsor
+    mapping(address => address) public sponsors;
+
+    // mapping of the sponsored addresses to caller address to the counter of
+    // the made free transactions
+    mapping(address => mapping(address => uint256)) public txCount;
+
+    // mapping of the sponsored funds to the sponsored address
+    // mapping(address => uint256) public sponsoredFunds;
 
     modifier onlyBootloader() {
         require(
             msg.sender == BOOTLOADER_FORMAL_ADDRESS,
-            "Only bootloader can call this method"
+            'Only bootloader can call this method'
         );
         // Continure execution if called from the bootloader.
         _;
@@ -30,31 +39,101 @@ contract Paymaster is IPaymaster {
         Transaction calldata _transaction
     ) external payable override onlyBootloader returns (bytes memory context) {
         require(
-            _transaction.paymasterInput.length >= 4,
-            "The standard paymaster input must be at least 4 bytes long"
+            nFirstTx[_uintToAddr(_transaction.to)] >
+                txCount[_uintToAddr(_transaction.to)][
+                    _uintToAddr(_transaction.from)
+                ],
+            'Paymaster: you reached your free tx limit'
         );
+
+        require(
+            _transaction.paymasterInput.length >= 4,
+            'Paymaster: The standard paymaster input must be at least 4 bytes long'
+        );
+
 
         bytes4 paymasterInputSelector = bytes4(
             _transaction.paymasterInput[0:4]
         );
-        if (paymasterInputSelector == IPaymasterFlow.approvalBased.selector) {
-            require(TX_COUNT < N_FIRST_TX, "Too many transactions");
-
+        if (paymasterInputSelector == IPaymasterFlow.general.selector) {
             // Note, that while the minimal amount of ETH needed is tx.ergsPrice * tx.ergsLimit,
             // neither paymaster nor account are allowed to access this context variable.
-            uint256 requiredETH = _transaction.ergsLimit *
-                _transaction.maxFeePerErg;
 
+            uint256 requiredETH = _transaction.ergsLimit *
+                        _transaction.maxFeePerErg;
             // The bootloader never returns any data, so it can safely be ignored here.
             (bool success, ) = payable(BOOTLOADER_FORMAL_ADDRESS).call{
                 value: requiredETH
-            }("");
-
-            require(success, "Failed to transfer funds to the bootloader");
-            TX_COUNT++;
+            }('');
+            require(
+                success,
+                'Paymaster: Failed to transfer funds to the bootloader'
+            );
+            txCount[_uintToAddr(_transaction.to)][
+                _uintToAddr(_transaction.from)
+            ]++;
         } else {
-            revert("Unsupported paymaster flow");
+            revert('Unsupported paymaster flow');
         }
+    }
+
+    function getMyCount(address _spnosorredAddr)
+        external
+        view
+        returns (uint256)
+    {
+        return txCount[_spnosorredAddr][msg.sender];
+    }
+
+    function getSponsor(address _spnosorredAddr)
+        external
+        view
+        returns (address)
+    {
+        return sponsors[_spnosorredAddr];
+    }
+
+    function sponsorTheAddress(address _addr, uint256 _n) external payable {
+        require(
+            nFirstTx[_addr] == 0 || sponsors[_addr] == msg.sender,
+            'Paymaster: sponsor already set'
+        );
+
+        require(
+            _n > 0,
+            'Paymaster: The number of free transactions must be greater than 0'
+        );
+
+        nFirstTx[_addr] = _n;
+        sponsors[_addr] = msg.sender;
+        // sponsoredFunds[_addr] += msg.value;
+    }
+
+    function stopSponsorship(address _addr) external {
+        require(
+            sponsors[_addr] == msg.sender,
+            'Paymaster: you are not a sponsor of this address'
+        );
+
+        nFirstTx[_addr] = 0;
+        sponsors[_addr] = address(0);
+        // uint256 refund = sponsoredFunds[_addr];
+        // sponsoredFunds[_addr] = 0;
+
+        // (bool success, ) = payable(address(msg.sender)).call{value: refund}('');
+
+        // require(
+        //     success,
+        //     'Paymaster: Failed to transfer remaining funds to the sponsor'
+        // );
+    }
+
+    function _addrToUint256(address a) internal pure returns (uint256) {
+        return uint256(uint160(a));
+    }
+
+    function _uintToAddr(uint256 a) internal pure returns (address) {
+        return address(uint160(a));
     }
 
     function postOp(
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,109 +1,33 @@
-import { config as dotenvConfig } from "dotenv";
-import {
-  NetworkUserConfig,
-  HttpNetworkAccountsUserConfig,
-} from "hardhat/types";
-dotenvConfig();
+import { config as dotenvConfig } from 'dotenv'
 
-require("@matterlabs/hardhat-zksync-deploy");
-require("@matterlabs/hardhat-zksync-solc");
-
-type NetworkName = string;
+import '@matterlabs/hardhat-zksync-deploy'
+import '@matterlabs/hardhat-zksync-solc'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomicfoundation/hardhat-chai-matchers'
+dotenvConfig()
 
 const zkSyncDeploy =
-  process.env.NODE_ENV === "test"
+  process.env.NODE_ENV === 'test'
     ? {
-        zkSyncNetwork: "http://localhost:3050",
-        ethNetwork: "http://localhost:8545",
+        zkSyncNetwork: 'http://localhost:3050',
+        ethNetwork: 'http://localhost:8545',
       }
     : {
-        zkSyncNetwork: "https://zksync2-testnet.zksync.dev",
+        zkSyncNetwork: 'https://zksync2-testnet.zksync.dev',
         ethNetwork: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      };
-
-const INFURA_ENDPOINTS: { [name: string]: string } = {
-  mainnet: "https://mainnet.infura.io/v3/",
-  rinkeby: "https://rinkeby.infura.io/v3/",
-  goerli: "https://goerli.infura.io/v3/",
-  kovan: "https://kovan.infura.io/v3/",
-  ropsten: "https://ropsten.infura.io/v3/",
-
-  polygon: "https://polygon-mainnet.infura.io/v3/",
-  mumbai: "https://polygon-mumbai.infura.io/v3/",
-};
-
-const CHAIN_IDS: { [name: string]: number } = {
-  ganache: 1337,
-  goerli: 5,
-  hardhat: 31337,
-  kovan: 42,
-  mainnet: 1,
-  mumbai: 80001,
-  polygon: 137,
-  rinkeby: 4,
-  ropsten: 3,
-};
-
-function getAccounts(network: string): HttpNetworkAccountsUserConfig {
-  if (process.env.PRIVATE_KEY) {
-    return [process.env.PRIVATE_KEY];
-  }
-  return {
-    count: 5,
-    initialIndex: 0,
-    mnemonic: getMnemonic(network),
-    path: "m/44'/60'/0'/0",
-  };
-}
-
-function createNetworkConfig(
-  network: string,
-  extraOpts = {}
-): NetworkUserConfig {
-  return Object.assign(
-    {
-      accounts: getAccounts(network),
-      // @ts-ignore
-      chainId: CHAIN_IDS[network],
-      timeout: 99999,
-      url: getRpcUrl(network),
-    },
-    extraOpts
-  );
-}
-
-function getMnemonic(network: NetworkName): string {
-  if (process.env.HARDHAT_NO_MNEMONIC) {
-    // dummy mnemonic
-    return "any pig at zoo eat toy now ten men see job run";
-  }
-  if (process.env.MNEMONIC) return process.env.MNEMONIC;
-  try {
-    return require("./mnemonic.js");
-  } catch (error) {
-    throw new Error(`Please set your MNEMONIC (for network: ${network})`);
-  }
-}
-
-function getRpcUrl(network: NetworkName): string {
-  if (process.env.INFURA_API_KEY && INFURA_ENDPOINTS[network])
-    return INFURA_ENDPOINTS[network] + process.env.INFURA_API_KEY;
-  if (network === "mumbai") return "https://rpc-mumbai.maticvigil.com/";
-  if (network === "polygon") return "https://polygon-rpc.com/";
-  return "undefined RPC provider URL";
-}
+      }
 
 module.exports = {
   zksolc: {
-    version: "1.2.0",
-    compilerSource: "binary",
+    version: '1.2.0',
+    compilerSource: 'binary',
     settings: {
       optimizer: {
         enabled: true,
       },
       experimental: {
-        dockerImage: "matterlabs/zksolc",
-        tag: "v1.2.0",
+        dockerImage: 'matterlabs/zksolc',
+        tag: 'v1.2.0',
       },
     },
   },
@@ -112,9 +36,8 @@ module.exports = {
     hardhat: {
       zksync: true,
     },
-    goerli: createNetworkConfig("goerli"),
   },
   solidity: {
-    version: "0.8.16",
+    version: '0.8.16',
   },
-};
+}
